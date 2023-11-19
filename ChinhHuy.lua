4520a0b2d871902faf4e912e12968c8ddc1a40ba
theaug <93984863+vuthaison1308@users.noreply.github.com>
2023-11-19 14:21:57 +0700

    dung bug nua lam on

--- ChinhHuy.lua
+++ ChinhHuy.lua
@@ -1,8 +1,8 @@
 spawn(function()
     repeat wait() until game:IsLoaded()
     if _G.JoinTeam == nil then
-        _G.JoinTeam = "Pirate"
-        print"Pirate"
+        _G.JoinTeam = "Pirate" 
+        print"pirate"
     end
     ChooseTeam = game:GetService("Players").LocalPlayer.PlayerGui:FindFirstChild("ChooseTeam",true)
     UIController = game:GetService("Players").LocalPlayer.PlayerGui:FindFirstChild("UIController",true)
@@ -10,8 +10,8 @@ spawn(function()
         print"true"
         for i,v in pairs(getgc()) do
             if type(v) == "function" and getfenv(v).script == UIController then
-                local constant = getconstants(v)
                 print"ok"
+                local constant = getconstants(v)
                 pcall(function()
                     if constant[1] == _G.JoinTeam.."s" and #constant == 1 then
                         v(_G.JoinTeam.."s")
@@ -20,10 +20,5 @@ spawn(function()
             end
         end
     end
-)
-spawn(function()
-    while wait(2) do
-        game:GetService("StarterGui"):SetCore("SendNotification", {Title = "Vu Thai Son", Text = "https://www.facebook.com/theaug1308"})
-        print"ok"
-    end
+    print"ended"
 )
