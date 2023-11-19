98fc91ece305a2f86a600cccd59b55c8c1b8b7e4
theaug <93984863+vuthaison1308@users.noreply.github.com>
2023-11-19 14:18:16 +0700

    lan cuoi r nhe troi oi

--- ChinhHuy.lua
+++ ChinhHuy.lua
@@ -1,20 +1,7 @@
 spawn(function()
+    repeat wait() until game:IsLoaded()
     if _G.JoinTeam == nil then
-        _G.JoinTeam = "Pirate"
-    end
-    repeat wait() until game.Players
-    repeat wait() until game.Players.LocalPlayer
-    repeat wait() until game.ReplicatedStorage
-    repeat wait() until game.ReplicatedStorage:FindFirstChild("Remotes");
-    repeat wait() until game:GetService("ReplicatedStorage").Effect.Container
-    repeat wait() until game.Players.LocalPlayer:FindFirstChild("PlayerGui");
-    repeat wait() until game.Players.LocalPlayer.PlayerGui:FindFirstChild("Main");
-    repeat wait() until game:GetService("Players")
-    repeat wait() until game:GetService("Players").LocalPlayer.Character:FindFirstChild("Energy")
-    wait(1)
-    repeat fask.wait()
-    if not game.Players.LocalPlayer.PlayerGui.Main:FindFirstChild("ChooseTeam") then
-        break;
+        _G.JoinTeam = "Pirate" 
     end
     ChooseTeam = game:GetService("Players").LocalPlayer.PlayerGui:FindFirstChild("ChooseTeam",true)
     UIController = game:GetService("Players").LocalPlayer.PlayerGui:FindFirstChild("UIController",true)
@@ -30,8 +17,6 @@ spawn(function()
             end
         end
     end
-    wait(1)
-until game.Players.LocalPlayer.Team ~= nil and game:IsLoaded()
 )
 spawn(function()
     while wait(2) do
